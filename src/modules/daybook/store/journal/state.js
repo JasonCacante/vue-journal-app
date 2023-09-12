eaf8cda3f30b15608152f08cdbed922af7b9bd0e
JasonCacante <jason.cacante@gmail.com>
2023-09-11 21:22:29 -0500

    build entries in state for journal component"

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(), // 1618315200000
+      date: new Date().toDateString(), // 'Mon Apr 12 2021'
+      text: 'Do pariatur commodo aliquip mollit cillum qui sint sit aliquip voluptate voluptate Lorem proident sint. Aliqua laboris dolor deserunt eu amet magna nulla aliqua Lorem ex laboris. Ut do nostrud ipsum esse nulla commodo dolore duis nostrud do pariatur nostrud. Incididunt anim eu ullamco dolor commodo eiusmod eu mollit proident occaecat officia.',
+      picture: null, // https://res.cloudinary.com/duzf4vfki/image/upload/v1618315200/daybook/1-1618315200.jpg
+    },
+    {
+      id: new Date().getTime() + 1000, // 1618315201000
+      date: new Date().toDateString(), // 'Mon Apr 12 2021'
+      text: 'Adipisicing dolore aliqua duis ea proident adipisicing veniam. Nulla occaecat mollit fugiat ea nostrud quis fugiat. Id veniam tempor sunt ad do adipisicing excepteur officia nulla ut cillum cillum fugiat. Minim laborum quis esse mollit aliquip quis. Proident deserunt anim deserunt quis sint ex dolor minim qui consectetur id labore. Aliquip minim anim veniam sint minim eu aliqua laborum velit sunt.',
+      picture: null, // https://res.cloudinary.com/duzf4vfki/image/upload/v1618315200/daybook/1-1618315200.jpg
+    },
+    {
+      id: new Date().getTime() + 1000, // 1618315201000
+      date: new Date().toDateString(), // 'Mon Apr 12 2021'
+      text: 'Non Lorem aute fugiat nulla minim consequat nostrud nisi voluptate consequat labore non consequat. Irure cupidatat anim cillum consectetur nulla pariatur aute ad officia quis dolor esse cupidatat. Sunt adipisicing anim incididunt veniam. Occaecat eiusmod eu anim duis consequat. Veniam non elit aute ea eu nisi non do est. Adipisicing enim fugiat ad eu cupidatat mollit officia nisi officia nulla aliqua. Incididunt Lorem irure do id amet nulla fugiat laboris mollit fugiat aliqua officia.',
+      picture: null, // https://res.cloudinary.com/duzf4vfki/image/upload/v1618315200/daybook/1-1618315200.jpg
+    },
+  ]
+})
